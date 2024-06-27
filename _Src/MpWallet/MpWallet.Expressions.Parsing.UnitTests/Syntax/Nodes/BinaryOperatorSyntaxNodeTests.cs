932be7c861a6791a784653e6159b7c1b1162b608
semikin.ia <semikin.ia@skbkontur.ru>
2024-06-27 21:40:47 +0300

    Additional BinaryOperatorSyntaxNode tests

--- _Src/MpWallet/MpWallet.Expressions.Parsing.UnitTests/Syntax/Nodes/BinaryOperatorSyntaxNodeTests.cs
+++ _Src/MpWallet/MpWallet.Expressions.Parsing.UnitTests/Syntax/Nodes/BinaryOperatorSyntaxNodeTests.cs
@@ -1,5 +1,6 @@
 ﻿using MpWallet.Expressions.Operators;
 using MpWallet.Expressions.Parsing.Syntax;
+using MpWallet.Expressions.Parsing.Syntax.Extensions;
 using MpWallet.Expressions.Parsing.Syntax.Nodes;
 using MpWallet.Expressions.Parsing.Syntax.Nodes.Abstractions;
 
@@ -7,30 +8,28 @@ namespace MpWallet.Expressions.Parsing.UnitTests.Syntax.Nodes;
 
 public sealed class BinaryOperatorSyntaxNodeTests
 {
-    private const string Input = "1 + 1";
+    private const string Input = "1 + 2";
     
     private static readonly Token Token;
+    private static readonly Operator Operator;
     private static readonly SyntaxNode LeftNode;
     private static readonly SyntaxNode RightNode;
 
     static BinaryOperatorSyntaxNodeTests()
     {
-        Token = new Token(Input, 0, Input.Length);
+        Token = new Token(Input, 2, 3);
+        Operator = new Operator("+", new OperatorDetails(0, OperatorAssociativity.Left, OperatorArity.Binary));
         LeftNode = new NumberSyntaxNode(new Token(Input, 0, 1));
         RightNode = new NumberSyntaxNode(new Token(Input, 4, Input.Length));
     }
 
-    public static IEnumerable<object[]> ConstructorSuccessCases =>
-        Operator.All.Select(@operator => new object[] { @operator });
-
-    [Theory]
-    [MemberData(nameof(ConstructorSuccessCases))]
-    public void Constructor_ShouldSuccess_WhenArgumentsCorrect(Operator @operator)
+    [Fact]
+    public void Constructor_ShouldSuccess_WhenArgumentsCorrect()
     {
-        var node = new BinaryOperatorSyntaxNode(Token, @operator, LeftNode, RightNode);
+        var node = new BinaryOperatorSyntaxNode(Token, Operator, LeftNode, RightNode);
         
         Assert.Equal(Token, node.Token);
-        Assert.Equal(@operator, node.Operator);
+        Assert.Equal(Operator, node.Operator);
         Assert.Equal(LeftNode, node.LeftOperand);
         Assert.Equal(RightNode, node.RightOperand);
     }
@@ -58,5 +57,82 @@ public sealed class BinaryOperatorSyntaxNodeTests
         Assert.NotNull(exception);
         Assert.IsType<ArgumentNullException>(exception);
         Assert.Equal(expectedName, ((ArgumentNullException)exception).ParamName);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentException_WhenOperatorIsNotBinary()
+    {
+        var @operator = new Operator("+", new OperatorDetails(0, OperatorAssociativity.Left, OperatorArity.Unary));
+
+        var exception = Record.Exception(() => new BinaryOperatorSyntaxNode(Token, @operator, LeftNode, RightNode));
+
+        Assert.NotNull(exception);
+        Assert.IsType<ArgumentException>(exception);
+        Assert.StartsWith("Operator must be binary", ((ArgumentException)exception).Message);
+        Assert.Equal("operator", ((ArgumentException)exception).ParamName);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowInvalidOperationException_WhenOperatorAndTokenNotEquals()
+    {
+        var @operator = new Operator("-", new OperatorDetails(0, OperatorAssociativity.Left, OperatorArity.Binary));
+
+        var exception = Record.Exception(() => new BinaryOperatorSyntaxNode(Token, @operator, LeftNode, RightNode));
+
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidOperationException>(exception);
+        Assert.Equal("Operator value and token value must equals", exception.Message);
+    }
+
+    public static TheoryData<Token, Operator, SyntaxNode, SyntaxNode> ConstructorInputsDifferentCases
+    {
+        get
+        {
+            {
+                var data = new TheoryData<Token, Operator, SyntaxNode, SyntaxNode>();
+
+                const string otherInput = "2 + 1";
+
+                var left = new NumberSyntaxNode(otherInput.ToToken(4, 5));
+                data.Add(Token, Operator, left, RightNode);
+
+                var right = new NumberSyntaxNode(otherInput.ToToken(0, 1));
+                data.Add(Token, Operator, LeftNode, right);
+                
+                return data;
+            }
+        }
+    }
+    
+    [Theory]
+    [MemberData(nameof(ConstructorInputsDifferentCases))]
+    public void Constructor_ShouldThrowInvalidOperationException_WhenInputsDifferent(
+        Token token, Operator @operator, SyntaxNode left, SyntaxNode right)
+    {
+        var exception = Record.Exception(() => new BinaryOperatorSyntaxNode(token, @operator, left, right));
+
+        Assert.NotNull(exception);
+        Assert.IsType<InvalidOperationException>(exception);
+        Assert.Equal("Inputs must be equals", ((InvalidOperationException)exception).Message);
+    }
+
+    public static TheoryData<SyntaxNode, SyntaxNode, string> ConstructorOperandOnIncorrectSideCases =>
+        new()
+        {
+            { LeftNode, LeftNode, "right" },
+            { RightNode, RightNode, "left" }
+        };
+    
+    [Theory]
+    [MemberData(nameof(ConstructorOperandOnIncorrectSideCases))]
+    public void Constructor_ShouldThrowArgumentException_WhenOperandOnIncorrectSide(
+        SyntaxNode left, SyntaxNode right, string parameterName)
+    {
+        var exception = Record.Exception(() => new BinaryOperatorSyntaxNode(Token, Operator, left, right));
+
+        Assert.NotNull(exception);
+        Assert.IsType<ArgumentException>(exception);
+        Assert.Contains("operator must be on", exception.Message);
+        Assert.Equal(parameterName, ((ArgumentException)exception).ParamName);
     }
 }
--- _Src/MpWallet/MpWallet.Expressions.Parsing/Syntax/Nodes/BinaryOperatorSyntaxNode.cs
+++ _Src/MpWallet/MpWallet.Expressions.Parsing/Syntax/Nodes/BinaryOperatorSyntaxNode.cs
@@ -15,7 +15,21 @@ public sealed record BinaryOperatorSyntaxNode : SyntaxNode
         ArgumentNullException.ThrowIfNull(@operator);
         ArgumentNullException.ThrowIfNull(left);
         ArgumentNullException.ThrowIfNull(right);
+        
+        if (@operator.Value != token.Value)
+            throw new InvalidOperationException("Operator value and token value must equals");
 
+        if (@operator.Details.Arity is not OperatorArity.Binary)
+            throw new ArgumentException("Operator must be binary", nameof(@operator));
+
+        if (token.Input != left.Token.Input || left.Token.Input != right.Token.Input)
+            throw new InvalidOperationException("Inputs must be equals");
+
+        if (left.Token.End >= token.Begin)
+            throw new ArgumentException("Left operator must be on left", nameof(left));
+        if (right.Token.Begin <= token.End)
+            throw new ArgumentException("Right operator must be on right", nameof(right));
+        
         Operator = @operator;
         LeftOperand = left;
         RightOperand = right;
